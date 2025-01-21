58d21e5629efd7b20fbdb9c183cc1516f44f3d8d
andreialex007 <andreialexandrovich@gmail.com>
2025-01-21 12:37:15 +0700

    back simplification

--- back/app/core/auth/router.py
+++ back/app/core/auth/router.py
@@ -12,39 +12,15 @@ router = APIRouter(
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = AuthUtils.decode(token)
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-    except JWTError:
-        raise credentials_exception
-    user = get_user(fake_users_db, username=username)
-    if user is None:
-        raise credentials_exception
-    return user
-
-
 @router.post("/token")
 async def login(
         form_data: OAuth2PasswordRequestForm = Depends(),
         user_service: UserService = Depends(get_user_service)
 ):
-    user = user_service.login(form_data.username, form_data.password)
+    user = await user_service.login(form_data.username, form_data.password)
     if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    access_token = AuthUtils.create_access_token(data={"name": user.username})
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED)
+    access_token = AuthUtils.create_access_token(data={"name": user.username, id: user.id})
     return {"access_token": access_token, "token_type": "bearer"}
 
 
@@ -53,7 +29,5 @@ async def current_user(
         token: str = Depends(oauth2_scheme),
         user_service: UserService = Depends(get_user_service)
 ):
-    payload = AuthUtils.decode(token)
-    username: str = payload.get("name")
-    user = user_service.get_by_name(username)
+    user = user_service.get_current_user(token)
     return user
--- back/app/core/users/user_service.py
+++ back/app/core/users/user_service.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 
 from common.database import get_db, User
 from common.models.service_base import ServiceBase
+from core.auth.auth_utils import AuthUtils
 from core.users.user_dto import UserDto
 
 
@@ -13,12 +14,20 @@ def get_user_service(db: Session = Depends(get_db)):
 
 class UserService(ServiceBase):
     async def get_by_id(self, id: int) -> UserDto:
-        user = (await self.db.execute(select(User).filter(User.id == id))).scalar_one()
+        user = ((await self.db.execute(select(User).filter(User.id == id)))
+                .scalar_one())
         return UserDto(id=user.id, name=user.name)
 
     async def get_by_name(self, name: str) -> UserDto:
-        user = (await self.db.execute(select(User).filter(User.name == name))).scalar_one()
+        user = ((await self.db.execute(select(User).filter(User.name == name)))
+                .scalar_one())
         return UserDto(id=user.id, name=user.name)
+
+    async def get_current_user(self, token: str) -> UserDto:
+        payload = AuthUtils.decode(token)
+        id: str = payload.get("id")
+        user = self.get_by_id(id)
+        return user
 
     async def login(self, name: str, password: str) -> UserDto:
         user = (await self.db.execute(select(User).filter(
