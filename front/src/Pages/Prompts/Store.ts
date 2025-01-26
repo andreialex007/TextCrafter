fdbaf32b7b708444cfbca3acef81e195c98cb0f3
andreialex007 <andreialexandrovich@gmail.com>
2025-01-26 17:19:38 +0700

    ui search

--- front/src/Pages/Prompts/Store.ts
+++ front/src/Pages/Prompts/Store.ts
@@ -1,14 +1,68 @@
 import { makeAutoObservable, makeObservable, observable } from 'mobx';
 import NavItem from '../../Common/NavItem.ts';
 import ItemEditStore from './Edit/Store.ts';
-import ItemsListStore from './List/Store.ts';
+
+type Category = {
+ id: number;
+ name: string;
+ description?: string;
+ prompts: Prompt[];
+};
+
+type Prompt = {
+ id: number;
+ name: string;
+ categoryId?: number;
+ content: string;
+};
 
 export default class Store extends NavItem {
  @observable
  loading = false;
 
+ @observable
  edit = new ItemEditStore();
- list = new ItemsListStore();
+
+ @observable
+ searchTerm = '';
+
+ @observable
+ categories: Array<Category> = [
+  {
+   id: 5,
+   name: 'first item',
+   description: 'first item description',
+   prompts: [
+    {
+     id: 1,
+     name: 'first prompt',
+     content: 'first prompt content',
+    },
+    {
+     id: 2,
+     name: 'first prompt',
+     content: 'first prompt content',
+    },
+   ],
+  },
+  {
+   id: 6,
+   name: 'second item',
+   description: 'second item description',
+   prompts: [
+    {
+     id: 3,
+     name: 'second prompt',
+     content: 'second prompt content',
+    },
+    {
+     id: 4,
+     name: 'second prompt',
+     content: 'third prompt content',
+    },
+   ],
+  },
+ ];
 
  constructor() {
   super();
@@ -18,4 +72,24 @@ export default class Store extends NavItem {
  name = 'Prompts';
  icon = 'discuss-fill';
  url = '/prompts';
+
+ get filteredCategories() {
+  return this.filterCategories(this.categories);
+ }
+
+ filterCategories = (categories: Array<Category>) => {
+  return categories.filter(
+   (x) =>
+    x.name.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+    this.filterPrompts(x.prompts).some((x) => x),
+  );
+ };
+
+ filterPrompts = (prompts: Array<Prompt>) => {
+  return prompts.filter(
+   (x) =>
+    x.name.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+    x.content.toLowerCase().includes(this.searchTerm.toLowerCase()),
+  );
+ };
 }
--- front/src/Pages/Prompts/index.tsx
+++ front/src/Pages/Prompts/index.tsx
@@ -4,13 +4,97 @@ import Store from './Store';
 import { Route, Switch } from 'wouter';
 import Page404 from '@/Common/Page404';
 import Edit from './Edit';
-import List from './List';
+
+const highlightSearchTerm = (text: string, searchTerm: string) => {
+ if (!searchTerm) return text;
+
+ const regex = new RegExp(`(${searchTerm})`, 'gi');
+ const parts = text.split(regex);
+
+ return parts.map((part, index) =>
+  regex.test(part) ? (
+   <span key={index} style={{ backgroundColor: 'lightblue' }}>
+    {part}
+   </span>
+  ) : (
+   part
+  ),
+ );
+};
 
 export default observer(({ store }: { store: Store }) => {
+ const hasSearchTerm = store.searchTerm.trim() !== '';
+ const hasFilteredCategories = store.filteredCategories.length > 0;
+
  return (
   <Switch>
    <Route path="/">
-    <List store={store.list} />
+    <div className="basic-page full flex flex-col">
+     <div className="flex flex-col gap-2">
+      <div className="relative flex-1">
+       <input
+        type="text"
+        placeholder="Search..."
+        className="focus:ring-azure-600 w-full rounded-lg border border-gray-300 p-2 pl-10 focus:outline-none focus:ring-2"
+        value={store.searchTerm}
+        onChange={(e) => (store.searchTerm = e.target.value)}
+       />
+       <i className="ri-search-line absolute left-3 top-1/2 -translate-y-1/2 transform text-gray-400"></i>
+      </div>
+      <div className="basic-btn self-end bg-emerald-600 text-white ">
+       <i className="ri-add-box-fill"></i>
+       Add new category
+      </div>
+      {hasFilteredCategories ? (
+       store.filteredCategories.map((c) => (
+        <div key={c.id} className="group/main flex flex-col">
+         <div className="relative flex gap-3">
+          <div className="flex w-fit rounded bg-gray-500 px-2 font-bold text-white shadow">
+           {highlightSearchTerm(c.name, store.searchTerm)}
+          </div>
+          <div className="flex- invisible float-end flex gap-1 group-hover/main:visible">
+           <span className="basic-btn flex gap-1 bg-green-700 text-white">
+            <i className="ri-add-box-fill"></i> add
+           </span>
+           <span className="basic-btn flex gap-1 bg-blue-700 text-white">
+            <i className="ri-edit-fill"></i> edit
+           </span>
+           <span className="basic-btn flex-gap-1 bg-red-700 text-white">
+            <i className="ri-delete-bin-fill"></i>
+            del
+           </span>
+          </div>
+         </div>
+         <div className=" flex flex-col ">
+          {c.prompts?.map((p) => (
+           <div className="group relative z-20 flex w-full cursor-pointer gap-2 border pl-2 shadow-inner odd:bg-gray-100 even:bg-yellow-50 hover:opacity-60">
+            <span className="text-nowrap font-bold text-orange-800">
+             {highlightSearchTerm(p.name, store.searchTerm)}
+            </span>
+            <span className="truncate italic text-gray-600">
+             {highlightSearchTerm(p.content, store.searchTerm)}
+            </span>
+            <div className="invisible absolute right-0 top-0 flex flex-row gap-1 group-hover:visible">
+             <span className="basic-btn flex gap-1 bg-blue-700 text-white">
+              <i className="ri-edit-fill"></i> edit
+             </span>
+             <span className="basic-btn flex-gap-1 bg-red-700 text-white">
+              <i className="ri-delete-bin-fill"></i>
+              del
+             </span>
+            </div>
+           </div>
+          ))}
+         </div>
+        </div>
+       ))
+      ) : hasSearchTerm ? (
+       <div className="text-center text-gray-500">No items were found</div>
+      ) : (
+       <div className="text-center text-gray-500">No items</div>
+      )}
+     </div>
+    </div>
    </Route>
    <Route path="/:id">
     <Edit store={store.edit} />
